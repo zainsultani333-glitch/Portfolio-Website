b983c3dc43d5aa4b81ea41d031a6e2d73719b449
Your Name <your@email.com>
2026-04-25 09:49:56 +0500

    g

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import NotFound from "./pages/NotFound";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
